69ccbfe75bb80583edd41a567e3bc10806574eb6
suhaTaher <74402078+suhaTaher@users.noreply.github.com>
2023-03-30 03:27:51 +0300

    Pro

---
+++ HijawiCampany/src/hijawicampany/HijawiCampany.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hijawicampany;
+
+/**
+ *
+ * @author ASUS
+ */
+public class HijawiCampany {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+             LogIn login=new LogIn (); 
+            login.setVisible(true);
+            
+        // TODO code application logic here
+    }
+    
+}
